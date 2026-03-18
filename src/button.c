34e1561a26ec053141da364ef2d5e0c2d2de6e2d
GlitzyIsBored <dragonstudio209@gmail.com>
2026-03-18 20:40:03 +1100

    add outline

--- src/button.c
+++ src/button.c
@@ -6,13 +6,24 @@ Button button;
 void setButton(void) {
     button.madoDAME = LoadTexture(Mado);
    // button.madoMURI = LoadTexture(Mado2);
-   // button.Pinkish = LoadTexture(Outline); // I need to com e up with a better naming scheme lol
+    button.Pinkish = LoadTexture(Outline); // I need to com e up with a better naming scheme lol
+    button.btnState = 0;
+    button.btnAction = false;
+    button.mousePos = (Vector2){ 0.0f, 0.0f };
+    button.btnHeight = (float)button.madoDAME.height / 3;
+    button.btnBounds = (Rectangle){ 300 / 2.0f - button.madoDAME.width/2.0f, 300 / 2.0f - (float)button.madoDAME.height / 3 /2.0f, (float)button.madoDAME.width, button.btnHeight };
 }
 
 void drawButton(void) {
+    button.mousePos = GetMousePosition();
+    
+    if (CheckCollisionPointRec(button.mousePos, button.btnBounds)) {
+        DrawTexture(button.Pinkish, 0, 0, WHITE);      
+    }
     DrawTexture(button.madoDAME, 0, 0, WHITE);
 }
 
 void unloadButton(void) {
     UnloadTexture(button.madoDAME);
+    UnloadTexture(button.Pinkish);
 }
--- src/button.h
+++ src/button.h
@@ -1,12 +1,22 @@
-#ifndef BUTTON.H
-#define BUTTON.H
+#ifndef BUTTON_H
+#define BUTTON_H
 
 #define Mado "../images/DAME.png"
 #define Mado2 "../images/MURI.png"
 #define Outline "../images/PinkHalo.png"
 
+#define Voice1 "../images/DAME.png"
+#define Voice2 "../audios/MURI.WAV"
+
 typedef struct Button {
-    Texture2D madoDAME, madoMURI, Pinkish
+    Texture2D madoDAME, madoMURI, Pinkish;
+    Sound DAME, MURI;
+    
+    Rectangle btnBounds;
+    Vector2 mousePos;
+    bool btnAction;
+    int btnState;
+    float btnHeight;
 } Button;
 
 extern Button buttonState;
